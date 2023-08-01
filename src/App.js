2d02573ae0f3e82501e5d9b363c061ce7804b008
Bahar <bahar@regentmarkets.com>
2023-08-01 11:37:30 +0800

    LAYOUT-COMPONENT:Split-Screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
 
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
+};
+const RightHandComponent = ({ message }) => {
+  return <p style={{ backgroundColor: "red" }}>{message}!</p>;
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Bahar" />
+      <RightHandComponent message="Hello" />
+    </SplitScreen>
   );
 }
 
